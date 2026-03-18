a18364ad8fd422b1599c5ba72d2db581e195c097
theonlyronsonik-lab <theonlyronsonik@gmail.com>
2026-03-18 19:00:41 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -309,11 +309,9 @@ def is_high_quality(trend_aligned):
 # ─────────────────────────────────────────────
 
 def get_data(symbol):
-    url = (f"https://api.twelvedata.com"
-           f"?symbol={symbol}&interval={INTERVAL}&outputsize=210&apikey={API_KEY_1}")
-    response = requests.get(url)
-            
-    try:
+    url = (f"https://api.twelvedata.com/time_series"
+           f"?symbol={symbol}&interval={INTERVAL}&outputsize=210&apikey={API_KEY_1}"))
+            try:
         r = requests.get(url, timeout=15).json()
     except Exception as e:
         print(f"Fetch error {symbol}: {e}")
