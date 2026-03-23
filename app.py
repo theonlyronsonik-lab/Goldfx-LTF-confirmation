d03141200bb77de3cf4e67a5850c2cac81c6e758
theonlyronsonik-lab <theonlyronsonik@gmail.com>
2026-03-23 23:37:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,10 @@ from datetime import datetime, timezone
 SIGNALS_FILE = "signals.json"
 
 PIP_SIZES = {
-    "XAU/USD": 0.1,
+     "XAU/USD": 0.1,
     "EUR/USD": 0.0001,
+    "S&P 500":     0.01,
+    "CAD/JPY":     0.01,
 }
 
 LOT_SIZE = 0.01
