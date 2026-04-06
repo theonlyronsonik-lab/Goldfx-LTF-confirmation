2ebca45fe4e648d0901b9ded36cbf271ed97a02a
theonlyronsonik-lab <theonlyronsonik@gmail.com>
2026-04-06 13:16:06 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -27,9 +27,9 @@ SMTP_PASS   = os.getenv("SMTP_PASS", "")
 ALERT_EMAIL = os.getenv("ALERT_EMAIL", "")
 
 SYMBOLS  = ["XAU/USD", "EUR/USD", "EUR/JPY", "GBP/USD" , "GBP/JPY"]
-INTERVAL = "1min"
+INTERVAL = "5min"
 
-COOLDOWN_MINUTES = 5
+COOLDOWN_MINUTES = 15
 
 RSI_OVERBOUGHT = 70
 RSI_OVERSOLD   = 30
@@ -976,11 +976,11 @@ async def main():
                         last_div_time.setdefault(symbol, {})["BEAR"] = bear_candle_dt
 
             save_state(sess_on, sessions)
-            await asyncio.sleep(30)
+            await asyncio.sleep(300)
 
         except Exception as e:
             print(f"Runtime error: {e}")
-            await asyncio.sleep(60)
+            await asyncio.sleep(120)
 
 
 if __name__ == "__main__":
